57f68af72594c0e6b2e005c7e6044ec33a3c8761
Asmita Gaire <asmitagaire00@gmail.com>
2025-02-21 14:53:58 +1100

    get all user using prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,3 +5,5 @@ const prisma = new PrismaClient();
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
